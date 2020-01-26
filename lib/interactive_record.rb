9ad465594acb39b5b192da4024e7e5cee99cae5f
Scott Hoisington <hoyzington@gmail.com>
2020-01-26 00:35:34 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -4,9 +4,11 @@ require 'active_support/inflector'
 class InteractiveRecord
   
   def self.table_name
-    self.class.to_s.downcase.pluralize
+    self.to_s.downcase.pluralize
   end
   
-  
+  def self.column_names
+    
+  end
   
 end
