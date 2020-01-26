c551ef25b97eeb6422ed34814be16db67c0a2200
Scott Hoisington <hoyzington@gmail.com>
2020-01-26 01:04:58 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -24,7 +24,7 @@ class InteractiveRecord
   end
 
   def col_names_for_insert
-    self.class.column_names
+    self.class.column_names.delete_if {|col| col.name == 'id'}
   end
 
   def values_for_insert
